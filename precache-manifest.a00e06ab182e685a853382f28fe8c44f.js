97f80cbeabc09bbc6f82427692409c0cf3f418b5
Ritesh <catalight.github.io>
2019-11-22 20:21:06 -0500

    Updates

---
+++ precache-manifest.a00e06ab182e685a853382f28fe8c44f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e94aed367ef333015b036fcfcb564c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0e8c2b8b6231846a8707",
+    "url": "/react-app/static/css/main.e3a5d5b3.chunk.css"
+  },
+  {
+    "revision": "f933d62b0c828927c0f1",
+    "url": "/react-app/static/js/2.ec976ee4.chunk.js"
+  },
+  {
+    "revision": "0e8c2b8b6231846a8707",
+    "url": "/react-app/static/js/main.6e95341a.chunk.js"
+  },
+  {
+    "revision": "32ad91bada991c7cb304",
+    "url": "/react-app/static/js/runtime-main.56efc31c.js"
+  },
+  {
+    "revision": "dff0fa0a5db147915cdb4d9887893915",
+    "url": "/react-app/static/media/errorImg.dff0fa0a.jpg"
+  }
+]);
