c7a480e140d4a4d2596abcac6795227276595572
Ritesh Maharjan <rite_maharjan@yahoo.com>
2021-03-11 15:20:27 -0500

    Updates

---
+++ precache-manifest.e073a0330bdecb0bd62acf88baeed352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8362515e72725fb504e3",
+    "url": "/photo-gallery-app/static/css/main.cc0953d2.chunk.css"
+  },
+  {
+    "revision": "8362515e72725fb504e3",
+    "url": "/photo-gallery-app/static/js/main.4524f2c1.chunk.js"
+  },
+  {
+    "revision": "d52888f51a0d45f7af53",
+    "url": "/photo-gallery-app/static/js/runtime~main.751dc7f6.js"
+  },
+  {
+    "revision": "da07ba33f257014bfffa",
+    "url": "/photo-gallery-app/static/js/2.8384a857.chunk.js"
+  },
+  {
+    "revision": "dff0fa0a5db147915cdb4d9887893915",
+    "url": "/photo-gallery-app/static/media/errorImg.dff0fa0a.jpg"
+  },
+  {
+    "revision": "252a9407437dd621ec2938e7530eaa5d",
+    "url": "/photo-gallery-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.fc7b832283f646c1b23f272e9697cf07.js"
+  "/photo-gallery-app/precache-manifest.e073a0330bdecb0bd62acf88baeed352.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
+workbox.routing.registerNavigationRoute("/photo-gallery-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
